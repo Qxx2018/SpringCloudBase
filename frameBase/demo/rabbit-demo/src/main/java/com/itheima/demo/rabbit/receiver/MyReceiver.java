76946e5377d7706deda74c58a34580dbfc9ebdfd
XinXingQian <1044582360@qq.com>
2023-02-01 10:53:51 +0800

    @StreamListener 监听

--- frameBase/demo/rabbit-demo/src/main/java/com/itheima/demo/rabbit/receiver/MyReceiver.java
+++ frameBase/demo/rabbit-demo/src/main/java/com/itheima/demo/rabbit/receiver/MyReceiver.java
@@ -23,11 +23,11 @@ public class MyReceiver {
      * 测试消息监听
      */
     @StreamListener(value = MyChannel.INPUT,condition = "headers['mq-rabbit-msg-add']=='add-msg'")
-    public void testHandlerMsg(
-            Message<String> msg) throws IOException {
+    public void testHandlerMsg(Message<String> msg) throws IOException, RabbitMQException {
         log.info("mq 消息监听，接受消息=>{}",msg.toString());
         PayLoadDTO<String> payLoadDTO = JSONObject.parseObject(msg.getPayload(), new TypeReference<PayLoadDTO<String>>() {});
         log.info("消息队列事件"+payLoadDTO.getAction().toString());
         log.info("消息队列数据"+payLoadDTO.getData());
+        throw new RabbitMQException("消息消费异常");
     }
 }
