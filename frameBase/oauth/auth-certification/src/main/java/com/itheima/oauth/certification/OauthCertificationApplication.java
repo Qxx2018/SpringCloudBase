ea5a4426dd894e2ee08a58878852ba4b3c7a9518
XinXingQian <1044582360@qq.com>
2023-03-26 14:43:30 +0800

    feature: oauth-certification认证服务初始化

---
+++ frameBase/oauth/auth-certification/src/main/java/com/itheima/oauth/certification/OauthCertificationApplication.java
@@ -0,0 +1,21 @@
+package com.itheima.oauth.certification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author 10445
+ */
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages = {"com.itheima"})
+@EnableConfigurationProperties
+public class OauthCertificationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OauthCertificationApplication.class,args);
+        System.out.println("=======================OauthCertificationApplication Start ==================================");
+    }
+}
